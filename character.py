972621b8621693dca5dee4a68cb9b1d652cd14a7
Christopher Cuadrado <cuadradochris@hotmail.com>
2023-05-17 13:05:40 -0400

    initial collision

--- character.py
+++ character.py
@@ -21,7 +21,7 @@ class Character():
             0, 0, constants.TILE_SIZE * size, constants.TILE_SIZE * size)
         self.rect.center = (x, y)
 
-    def move(self, dx, dy):
+    def move(self, dx, dy, obstacle_tiles):
         screen_scroll = [0, 0]
 
         self.running = False
@@ -39,8 +39,27 @@ class Character():
             dx = dx * (math.sqrt(2)/2)
             dy = dy * (math.sqrt(2)/2)
 
+        # check for collision with map in x direction
         self.rect.x += dx
+        for obstacle in obstacle_tiles:
+            # check for collision
+            if obstacle[1].colliderect(self.rect):
+                # check which side direction is from
+                if dx > 0:
+                    self.rect.right = obstacle[1].left
+                if dx < 0:
+                    self.rect.left = obstacle[1].right
+
+        # check for collision with map in y direction
         self.rect.y += dy
+        for obstacle in obstacle_tiles:
+            # check for collision
+            if obstacle[1].colliderect(self.rect):
+                # check which side direction is from
+                if dy > 0:
+                    self.rect.bottom = obstacle[1].top
+                if dy < 0:
+                    self.rect.top = obstacle[1].bottom
 
         # logic only applies to player
         if self.char_type == 0:
